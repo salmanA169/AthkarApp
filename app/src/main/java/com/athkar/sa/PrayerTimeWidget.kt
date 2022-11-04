40ccb0308faddbfabcd76757819a649f213bf8f8
salmanA169 <40442174+salmanA169@users.noreply.github.com>
2022-11-04 23:28:42 +0300

    1- change background 2- add date and tv_today 3- implement PrayTimeWidget

--- app/src/main/java/com/athkar/sa/PrayerTimeWidget.kt
+++ app/src/main/java/com/athkar/sa/PrayerTimeWidget.kt
@@ -4,23 +4,37 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.util.Log
+import android.widget.ImageView
 import android.widget.RemoteViews
 import android.widget.Toast
+import androidx.core.os.BuildCompat
+import com.athkar.sa.uitls.ConstantPatternsDate
+import com.athkar.sa.uitls.getAlarmNotificationPendingIntent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import java.time.LocalDate
+import java.time.chrono.HijrahDate
 
-/**
- * Implementation of App Widget functionality.
- */
 class PrayerTimeWidget : AppWidgetProvider() {
+    private val TAG: String? = javaClass.simpleName
+    private val scope = CoroutineScope(SupervisorJob())
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
-
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
+    }
+
+    override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
+        super.onDeleted(context, appWidgetIds)
+        Log.d("PrayerTimeWidget", "onDeleted: called")
     }
 
     override fun onEnabled(context: Context) {
@@ -31,12 +45,20 @@ class PrayerTimeWidget : AppWidgetProvider() {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-}
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val dateEn = LocalDate.now()
+        val dateAr = HijrahDate.now()
+        val remoteViews = RemoteViews(context.packageName,R.layout.prayer_time_widget)
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
+        remoteViews.setTextViewText(R.id.tv_date_en,dateEn.toString())
+        remoteViews.setTextViewText(R.id.tv_date_ar,dateAr.format(ConstantPatternsDate.hijrahPattern))
+        remoteViews.setTextViewText(R.id.tv_today,dateAr.format(ConstantPatternsDate.todayPattern))
+
+
+        appWidgetManager.updateAppWidget(appWidgetId,remoteViews)
+    }
 }
